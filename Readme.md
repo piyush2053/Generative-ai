05ea294ed9eae05b61885abf351c9e0a24b05569
piyush.patel <piyush.patel@rws.com>
2024-06-24 20:36:11 +0530

    Readme alter

--- Readme.md
+++ Readme.md
@@ -1,8 +1,15 @@
 # P Gen - Generative AI 
 ![Screenshot](./assets//B.png)
 
+## Overview
+P-GEN is a generative AI API that can understand and respond to queries. It can also read images and answer questions about them.
+
+## Key Features:
+Query Response: Get intelligent responses to your text queries.
+Image Analysis: Upload images and receive detailed answers based on the image content.
+## Usage:
+Integrate P-GEN into your applications to enhance user interactions with advanced AI capabilities.
 
 ## Whom to Talk ?
-
 Piyush Patel
 piyushpatel2053@gmail.com
--- index.js
+++ index.js
@@ -2,58 +2,51 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { genAI } from './genAi.js';
 import cors from 'cors';
-import multer from 'multer';
-import path from 'path';
+
 
 const app = express();
 const port = process.env.PORT || 8090;
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname));
-    }
-});
-const upload = multer({ storage: storage });
 
 const handleError = (err, res) => {
     console.error(err);
     res.status(500).json({ message: 'Internal Server Error' });
 };
 
-async function generateAIResponse(query, imagePath = null) {
+
+async function generateAIResponse(query, imageUrl = null) {
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
 
-    const imageParts = [];
-    if (imagePath) {
-        imageParts.push(fileToGenerativePart(imagePath, "image/jpeg"));
-    }
+    // Note: The only accepted mime types are some image types, image/*.
+    // const imageParts = [
+    //     fileToGenerativePart("./utils/cat.jpg", "image/jpeg"),
+    //     fileToGenerativePart("./utils/scones.jpg", "image/jpeg"),
+    // ];
+
+    // const result = await model.generateContent([prompt, ...imageParts]);
+
 
     const { totalTokens } = await model.countTokens(query);
     console.log("Tokens count:", totalTokens);
-    const result = await model.generateContent([query, ...imageParts]);
+    const result = await model.generateContent(query);
     const response = result.response;
     const text = response.text();
-    return text;
+    return text
 }
 
-app.post('/askai', upload.single('image'), async (req, res) => {
-    const { query } = req.body;
-    const imagePath = req.file ? req.file.path : null;
+app.get('/askai', async (req, res) => {
+    const { query, imageUrl } = req.query;
 
     if (!query) {
         return res.status(400).json({ message: 'Missing required field: query' });
     }
 
     try {
-        const generatedResponse = await generateAIResponse(query, imagePath);
+        const generatedResponse = await generateAIResponse(query, imageUrl);
         res.json({ response: generatedResponse });
     } catch (error) {
         handleError(error, res);
